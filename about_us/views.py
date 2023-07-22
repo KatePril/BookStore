8f0f355d389fe0145493a26cdc7737b3ff9b49e6
KatePril <ket14pril14@gmail.com>
2023-07-22 21:14:44 +0300

    add about us page

--- about_us/views.py
+++ about_us/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def about_us(request):
-    return render(request, 'base.html')
+    return render(request, 'about_us/about_us.html')
